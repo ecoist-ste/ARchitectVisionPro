983c799d50b4550ef4cfa052465c9d2cf6022131
Songyuan Liu <lsystevee@gmail.com>
2025-03-20 15:00:04 -0500

    added tab view controller

---
+++ ARchitectVisionPro/Views/Main/MyProjectsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProjectsView.swift
+//  ARchitectVisionPro
+//
+//  Created by Songyuan Liu on 3/20/25.
+//
+
+import SwiftUI
+
+struct MyProjectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProjectsView()
+}
